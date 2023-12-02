e83458fefb21e46e97c61be92bfdead15cee5b82
LiamT8 <91737599+TackLiam@users.noreply.github.com>
2023-12-02 20:14:05 +0100

    Added Domain Models

---
+++ Hotel.Domain/Model/PriceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Domain.Model
+{
+    public class PriceInfo
+    {
+        int AdultPrice { get; set; }
+        int ChildPrice { get; set; }
+        int Discount { get; set; }
+        int AdultAge { get; set; }
+    }
+}
